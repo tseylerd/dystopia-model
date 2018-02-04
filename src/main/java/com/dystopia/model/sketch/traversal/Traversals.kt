4115a3e4e2f157c5f24a72c7fb699fdf45dda2d2
dmitriy.tseyler <dmitriy.tseyler@jetbrains.com>
2018-02-04 16:12:53 +0300

    cleanup

--- src/main/java/com/dystopia/model/sketch/traversal/Traversals.kt
+++ src/main/java/com/dystopia/model/sketch/traversal/Traversals.kt
@@ -5,13 +5,7 @@ import com.dystopia.model.sketch.api.SketchElement
 import com.dystopia.model.traversal.SimpleTraversalFactory
 
 object Traversals {
-    private var factory: SketchTraversalFactory = MockFactory
-
-    private object MockFactory: SketchTraversalFactory {
-        override fun traversalOf(clazz: Class<*>): SketchPairTraversal {
-            return MockTraversal
-        }
-    }
+    private var factory: SketchTraversalFactory = SimpleTraversalFactory
 
     object MockTraversal: SketchPairTraversal {
         override fun traverse(first: SketchElement, second: SketchElement, traverser: PairTransformingTraverser<*, SketchElement>): SketchElement? {
@@ -19,12 +13,7 @@ object Traversals {
         }
     }
 
-    fun setFactory(factory: SketchTraversalFactory) {
-        this.factory = factory
-    }
-
     fun traversalOf(element: SketchElement): SketchPairTraversal {
-        setFactory(SimpleTraversalFactory)
         return factory.traversalOf(element::class.java)
     }
 }
